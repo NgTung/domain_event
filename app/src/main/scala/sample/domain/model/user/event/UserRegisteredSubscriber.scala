92289baebf94cbc8d814ab74231f293615cfc436
Tung Nguyen Thanh <tung.nguyenthanh@grabtaxi.com>
2020-01-13 02:20:32 +0700

    refactor

--- app/src/main/scala/sample/domain/model/user/event/UserRegisteredSubscriber.scala
+++ app/src/main/scala/sample/domain/model/user/event/UserRegisteredSubscriber.scala
@@ -4,7 +4,8 @@ import src.main.scala.sample.infra.broker.EventSubscriber
 
 abstract class UserRegisteredSubscriber extends EventSubscriber[UserRegistered] {
   private def className[A](a: A)(implicit m: Manifest[A]) = m.toString
-  override def toString = className(this)
+
+  override def toString: String = className(this)
 
   override def handler(event: UserRegistered)
 }
